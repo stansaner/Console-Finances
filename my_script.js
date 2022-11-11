86a536906fea49635b348e8537edfec530e76e4c
Stan Saner <stan.saner@hotmail.co.uk>
2022-11-11 22:03:47 +0000

    Initial files and dataset

---
+++ my_script.js
@@ -0,0 +1,14 @@
+// Global variables
+
+
+
+// The actual code starts here
+// Print ttotal number of months included in the dataset
+
+console.log("Total number of months included in the dataset is "+finances.length);
+
+// Sum up net total amount of Profit/Losses over the whole period
+
+// for (var main_index = 0; main_index < finances.length; main_index++) {
+//
+// }    
